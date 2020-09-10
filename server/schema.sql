49abf696ebc7923c2f9e974e44a5a142cf9b8976
Claire Melbourne <claire.melbourne@gmail.com>
2020-09-10 13:23:03 -0500

    create tables and save to mysql with foreign key ids on messages

--- server/schema.sql
+++ server/schema.sql
@@ -1,8 +1,8 @@
-
+CREATE DATABASE chat;
 
 USE chat;
 
-/*CREATE TABLE users(
+CREATE TABLE users(
   ID INT AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(10) DEFAULT 'anonymous'
 );
@@ -10,19 +10,15 @@ USE chat;
 CREATE TABLE rooms(
   ID INT AUTO_INCREMENT PRIMARY KEY,
   room VARCHAR(10) DEFAULT 'lobby'
-);*/
+);
 
 CREATE TABLE messages(
   ID INT AUTO_INCREMENT PRIMARY KEY,
-  userMessaage VARCHAR(300),
-  usernameFK INT,
-  roomFK INT,
-  FOREIGN KEY (usernameFK)
-  REFERENCES users(ID)
-
-
-  FOREIGN KEY (roomFK)
-  REFERENCES rooms(ID)
+  userMessage VARCHAR(300),
+  username_id INT,
+  room_id INT,
+  FOREIGN KEY (username_id) REFERENCES users(ID),
+  FOREIGN KEY (room_id) REFERENCES rooms(ID)
 );
 
 
@@ -35,3 +31,15 @@ CREATE TABLE messages(
  */
 
 
+-- CREATE TABLE messages(
+--   ID INT AUTO_INCREMENT PRIMARY KEY,
+--   userMessaage VARCHAR(300),
+--   usernameFK INT,
+--   -- roomFK INT,
+--   FOREIGN KEY (usernameFK)
+--   REFERENCES users(ID)
+
+
+--   -- FOREIGN KEY (roomFK)
+--   -- REFERENCES rooms(ID)
+-- );
