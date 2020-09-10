5085b47b8706a27fbefc7b79a2520a3ce6f9c41a
vargas055 <juanjo220393@gmail.com>
2020-09-09 23:07:04 -0400

    Implemnted tables, bug on syntax

--- server/schema.sql
+++ server/schema.sql
@@ -1,17 +1,37 @@
-CREATE DATABASE chat;
+
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+/*CREATE TABLE users(
+  ID INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(10) DEFAULT 'anonymous'
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE rooms(
+  ID INT AUTO_INCREMENT PRIMARY KEY,
+  room VARCHAR(10) DEFAULT 'lobby'
+);*/
+
+CREATE TABLE messages(
+  ID INT AUTO_INCREMENT PRIMARY KEY,
+  userMessaage VARCHAR(300),
+  usernameFK INT,
+  roomFK INT,
+  FOREIGN KEY (usernameFK)
+  REFERENCES users(ID)
+
+
+  FOREIGN KEY (roomFK)
+  REFERENCES rooms(ID)
+);
+
 
 
 
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
- *  to create the database and the tables.*/
+ *  to create the database and the tables.
+ */
+
 
